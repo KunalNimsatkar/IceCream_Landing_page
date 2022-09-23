db29b20e24ed2e63f440711bb6d72b75c82af9e5
Kunal <kunal.nimsatkar@gmail.com>
2022-09-23 13:55:43 +0530

    changes in layout

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
-const humburger = document.querySelector('.menu');
-const mobileNavbar = document.querySelector('.navlinks');
-const menuBar = document.querySelector('.menuBar');
+const menu = document.getElementById("menu");
+const Navlinks = document.querySelector(".navlinks");
 
-const mobileNav = () => {
-   humburger.classList.toggle('fa-xmark');
-   mobileNavbar.classList.toggle('hidden');
-   mobileNavbar.classList.toggle('flex');
-   menuBar.classList.toggle('text-white');
+
+menu.onclick = () => {
+    menu.classList.toggle("openMenu");
+    Navlinks.classList.toggle("hidden")
 }
-humburger.addEventListener('click' , mobileNav);
-
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
